a8104690dcfe8bb2b42ebcfd979f480252cfaa2a
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-06-04 23:32:42 +0300

    create isTablet variable

--- app/utilities/Metrics.ts
+++ app/utilities/Metrics.ts
@@ -2,3 +2,13 @@ import {Dimensions} from 'react-native';
 
 export const deviceWidth = Dimensions.get('window').width;
 export const deviceHeight = Dimensions.get('window').height;
+
+const isTabletCheck = () => {
+  if (deviceHeight / deviceWidth > 1.6) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+export const isTablet = isTabletCheck();

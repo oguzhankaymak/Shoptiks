2018cbc9d4149d364275d01f890487331aae9bc4
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-06-04 23:35:20 +0300

    create login screen

--- app/containers/auth/login/Login.tsx
+++ app/containers/auth/login/Login.tsx
@@ -1,10 +1,51 @@
-import React, {FC} from 'react';
-import {View, Text} from 'react-native';
+import React, {FC, useState} from 'react';
+import {View, Text, TextInput} from 'react-native';
+
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import LargeButton from '../../../components/largeButton/LargeButton';
+
+import styles from './styles/Login.style';
 
 const Login: FC = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.titleText}>LOGIN</Text>
+      </View>
+      <KeyboardAwareScrollView enableOnAndroid={true} bounces={false}>
+        <View style={styles.formItem}>
+          <Text style={styles.labelText}>Email</Text>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter email"
+              value={email}
+              onChangeText={setEmail}
+            />
+          </View>
+        </View>
+        <View style={styles.formItem}>
+          <Text style={styles.labelText}>Password</Text>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter password"
+              value={password}
+              onChangeText={setPassword}
+            />
+          </View>
+        </View>
+        <LargeButton
+          text={'Login'}
+          loading={loading}
+          disabled={loading}
+          action={() => console.log('action')}
+        />
+      </KeyboardAwareScrollView>
     </View>
   );
 };
---
+++ app/containers/auth/login/styles/Login.style.ts
@@ -0,0 +1,48 @@
+import {StyleSheet} from 'react-native';
+import Colors from '../../../../theme/Colors';
+import Fonts from '../../../../theme/Fonts';
+import {deviceWidth, isTablet} from '../../../../utilities/Metrics';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    alignItems: 'center',
+  },
+
+  header: {
+    paddingTop: 100,
+  },
+
+  titleText: {
+    fontSize: Fonts.size.h23,
+    fontWeight: 'bold',
+  },
+
+  formItem: {
+    alignItems: 'center',
+    marginTop: 40,
+  },
+
+  inputView: {
+    width: deviceWidth - 85,
+    borderWidth: 1,
+    borderColor: Colors.lightBlue,
+    borderRadius: 50,
+    justifyContent: 'center',
+    paddingHorizontal: 3,
+    marginTop: 6,
+  },
+
+  labelText: {
+    fontSize: Fonts.size.h20,
+    color: Colors.gray,
+    fontWeight: 'bold',
+  },
+
+  input: {
+    fontSize: Fonts.size.h18,
+    color: Colors.gray,
+    padding: isTablet ? 20 : 8,
+  },
+});

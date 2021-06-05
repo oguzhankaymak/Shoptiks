75bafc2a371d7134cd9f0eed60e9c4547515341a
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-06-05 16:56:18 +0300

    authentication has been set in login screen with context api

--- app/containers/auth/login/Login.tsx
+++ app/containers/auth/login/Login.tsx
@@ -1,15 +1,21 @@
-import React, {FC, useState} from 'react';
-import {View, Text, TextInput} from 'react-native';
+import React, {FC, useState, useContext} from 'react';
+import {View, Text, TextInput, Alert} from 'react-native';
 
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import Button, {ButtonTypes} from '../../../components/button/Button';
 import {emailIsValid} from '../../../utilities/Function';
-import {INVALID_EMAIL} from '../../../constans/Messages';
+import {
+  INVALID_EMAIL,
+  INVALID_PASSWORD,
+  ALERT_TITLE_WARNING,
+} from '../../../constans/Messages';
 
 import styles from './styles/Login.style';
+import {GlobalContext} from '../../../context/Provider';
+import ActionLogin from '../../../context/auth/actions/ActionLogin';
 
 const Login: FC = () => {
-  const [loading, setLoading] = useState<boolean>(false);
+  const {authDispatch, authState} = useContext<any | null>(GlobalContext);
   const [email, setEmail] = useState<string>('');
   const [isValidEmail, setIsValidEmail] = useState<boolean>(false);
   const [password, setPassword] = useState<string>('');
@@ -21,6 +27,21 @@ const Login: FC = () => {
     } else {
       setIsValidEmail(false);
     }
+  };
+
+  const loginCheck = () => {
+    let errorMessage;
+    if (!email || email?.length === 0 || !isValidEmail) {
+      errorMessage = INVALID_EMAIL;
+    } else if (!password || password?.length === 0) {
+      errorMessage = INVALID_PASSWORD;
+    }
+
+    errorMessage
+      ? Alert.alert(ALERT_TITLE_WARNING + '!', errorMessage + '.', [
+          {text: 'Okay', onPress: () => {}},
+        ])
+      : ActionLogin()(authDispatch, email);
   };
 
   return (
@@ -61,9 +82,9 @@ const Login: FC = () => {
         <Button
           type={ButtonTypes.primary}
           text={'Login'}
-          loading={loading}
-          disabled={loading}
-          action={() => console.log('action')}
+          loading={authState.isLoading}
+          disabled={authState.isLoading}
+          action={loginCheck}
         />
       </KeyboardAwareScrollView>
     </View>

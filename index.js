1b7c0aa37d9da316a0135bcf440c918a24e14b97
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-06-05 23:44:46 +0300

    use redux on index

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ import {name as appName} from './app.json';
 import {NavigationContainer} from '@react-navigation/native';
 import Colors from './app/theme/Colors';
 import Navigation from './app/containers/Navigation';
+import {Provider} from 'react-redux';
+import {persistor, store} from './app/redux/Store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const Root: FC = () => (
   <SafeAreaView style={{flex: 1, backgroundColor: Colors.white}}>
-    <NavigationContainer>
-      <Navigation />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Navigation />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   </SafeAreaView>
 );
 

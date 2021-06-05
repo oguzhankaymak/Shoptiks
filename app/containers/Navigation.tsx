d4479e2391e307ea66a57df4058ed6b5a4c07298
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-06-05 18:45:32 +0300

    create main navigation stack and check user in navigation stack

--- app/containers/Navigation.tsx
+++ app/containers/Navigation.tsx
@@ -1,14 +1,21 @@
-import React, {FC} from 'react';
+import React, {FC, useContext} from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
 import AuthNavigation from './auth/AuthNavigation';
+import MainNavigation from './main/MainNavigation';
+import {GlobalContext} from '../context/Provider';
 
 const Stack = createStackNavigator();
 
 const Navigation: FC = () => {
+  const {authDispatch, authState} = useContext<any | null>(GlobalContext);
   return (
     <Stack.Navigator headerMode="none">
-      <Stack.Screen name="AuthNavigator" component={AuthNavigation} />
+      {authState.email ? (
+        <Stack.Screen name="MainNavigator" component={MainNavigation} />
+      ) : (
+        <Stack.Screen name="AuthNavigator" component={AuthNavigation} />
+      )}
     </Stack.Navigator>
   );
 };

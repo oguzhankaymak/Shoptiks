833cda02589e0498ee5b6c7844e3d3d203021674
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-06-06 23:22:33 +0300

    order must be complete so create check compete order function and complete order function

--- app/containers/bottomTab/cart/CartScreen.tsx
+++ app/containers/bottomTab/cart/CartScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Alert} from 'react-native';
 
 import styles from './styles/CartScreen.style';
 import {useAppDispatch, useAppSelector} from '../../../redux/Hooks';
@@ -7,10 +7,32 @@ import CartList from '../../../components/cartList/Index';
 import {ICartProduct} from '../../../redux/cart/initialState/InitialState';
 import {calcuateTotalPrice} from '../../../utilities/Function';
 import {deleteProductFromCartAction} from '../../../redux/cart/actions/Actions';
+import {CartActionTypes} from '../../../redux/cart/types/ActionTypes';
 
 const CartScreen = () => {
   const dispatch = useAppDispatch();
   const cart = useAppSelector(state => state.cartReducer.cartItem);
+
+  const checkCompeteOrder = () => {
+    return Alert.alert(
+      'Are You Sure?',
+      `The total price of your cart is ${calcuateTotalPrice(
+        cart,
+      )}$. Are you sure to complete your order?`,
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'Yes', onPress: () => completeOrder()},
+      ],
+    );
+  };
+
+  const completeOrder = () => {
+    dispatch({type: CartActionTypes.CLEAR_CART});
+  };
 
   const _renderContent = () => {
     if (cart && Array.isArray(cart) && cart.length > 0) {
@@ -37,7 +59,9 @@ const CartScreen = () => {
           <Text style={styles.totalPriceText}>
             Total: {calcuateTotalPrice(cart)}$
           </Text>
-          <TouchableOpacity style={styles.completeOrderButton}>
+          <TouchableOpacity
+            style={styles.completeOrderButton}
+            onPress={checkCompeteOrder}>
             <Text style={styles.completeOrderButtonText}>Complete Order</Text>
           </TouchableOpacity>
         </View>

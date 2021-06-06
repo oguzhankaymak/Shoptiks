6f6b36bfafb2579e2feaf9e6ce4353462aaabe90
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-06-07 01:13:31 +0300

    create product detail screen and add in homeStack

--- app/containers/bottomTab/homeStack/HomeStack.tsx
+++ app/containers/bottomTab/homeStack/HomeStack.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
+import ProductDetailScreen from './screens/ProductDetailScreen';
 
 const Stack = createStackNavigator();
 
@@ -8,6 +9,10 @@ const HomeStack: FC = () => {
   return (
     <Stack.Navigator headerMode={'none'}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen
+        name="ProductDetailScreen"
+        component={ProductDetailScreen}
+      />
     </Stack.Navigator>
   );
 };

4a96e87b4cabf4458e38efce624a3795c90b74c7
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-06-07 02:13:13 +0300

    create profile screen and implement in account stack

--- app/containers/bottomTab/accountStack/AccountStack.tsx
+++ app/containers/bottomTab/accountStack/AccountStack.tsx
@@ -2,6 +2,7 @@ import React, {FC} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import AccountScreen from './screens/AccountScreen';
 import LastOrdersScreen from './screens/LastOrdersScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ const AccountStack: FC = () => {
         options={{headerShown: false}}
       />
       <Stack.Screen name="LastOrdersScreen" component={LastOrdersScreen} />
+      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
     </Stack.Navigator>
   );
 };

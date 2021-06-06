3f46f247cd174af167a9448593a1e93b8f66aa43
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-06-07 02:12:43 +0300

    create last orders screen and implement in account stack

--- app/containers/bottomTab/accountStack/AccountStack.tsx
+++ app/containers/bottomTab/accountStack/AccountStack.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import AccountScreen from './screens/AccountScreen';
+import LastOrdersScreen from './screens/LastOrdersScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ const AccountStack: FC = () => {
         component={AccountScreen}
         options={{headerShown: false}}
       />
+      <Stack.Screen name="LastOrdersScreen" component={LastOrdersScreen} />
     </Stack.Navigator>
   );
 };
---
+++ app/containers/bottomTab/accountStack/screens/styles/LastOrdersScreen.style.ts
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+import Colors from '../../../../../theme/Colors';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    alignItems: 'center',
+    padding: 15,
+  },
+});

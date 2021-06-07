502db5992efac2da6332ca1517ba67864c5a03f3
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-06-07 10:18:09 +0300

    add logout button in account screen

--- app/containers/bottomTab/accountStack/screens/AccountScreen.tsx
+++ app/containers/bottomTab/accountStack/screens/AccountScreen.tsx
@@ -3,8 +3,22 @@ import {View, Text, TouchableOpacity} from 'react-native';
 
 import styles from './styles/AccountScreen.style';
 import {ArrowRightIcon} from '../../../../components/icons';
+import Button, {ButtonTypes} from '../../../../components/button/Button';
+import {useAppDispatch, useAppSelector} from '../../../../redux/Hooks';
+import {CartActionTypes} from '../../../../redux/cart/types/ActionTypes';
+import {AuthActionTypes} from '../../../../redux/auth/types/ActionTypes';
 
 const AccountScreen: FC<any> = ({navigation}) => {
+  const dispatch = useAppDispatch();
+  const cart = useAppSelector(state => state.cartReducer.cartItem);
+
+  const logout = () => {
+    if (cart && Array.isArray(cart) && cart.length > 0) {
+      dispatch({type: CartActionTypes.CLEAR_CART});
+    }
+    return dispatch({type: AuthActionTypes.LOGOUT});
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -19,16 +33,19 @@ const AccountScreen: FC<any> = ({navigation}) => {
             <ArrowRightIcon size={24} />
           </View>
         </TouchableOpacity>
-        <View style={styles.option}>
-          <TouchableOpacity
-            style={styles.shadow}
-            onPress={() => navigation.navigate('ProfileScreen')}>
-            <View style={styles.button}>
-              <Text>Profile</Text>
-              <ArrowRightIcon size={24} />
-            </View>
-          </TouchableOpacity>
-        </View>
+      </View>
+      <View style={styles.option}>
+        <TouchableOpacity
+          style={styles.shadow}
+          onPress={() => navigation.navigate('ProfileScreen')}>
+          <View style={styles.button}>
+            <Text>Profile</Text>
+            <ArrowRightIcon size={24} />
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.option}>
+        <Button type={ButtonTypes.danger} text={'Logout'} action={logout} />
       </View>
     </View>
   );
